ecece160facbc80498b537e5a71bae0c671226d4
Eduardo Neves10 <marcosdudu@icloud.com>
2025-04-17 22:41:33 -0300

    operadores-logicos

---
+++ operadores-logicos.js
@@ -0,0 +1,31 @@
+// and = e = && 
+// retorna true se ambos os operadores forem verdadeios
+
+let x = true
+let y = false
+console.log(x && y ) // false
+
+let a = false
+let b = false
+console.log(a && b) // false
+
+let d = true
+let e = true
+console.log(d && e) // true
+
+// or = ou = || talp
+// retorna true se pelo menos uma condição for verdadeira
+
+let z = 5
+
+console.log(z > 4 || z < 10) // true
+console.log(z > 6 || z < 4) // false
+
+// not = !x = diferente
+// inverte o valor lógico
+
+let u = true
+let r = false
+
+console.log(!u) // false
+console.log(!r) // true
